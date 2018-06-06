9012c1890ce0c9a30a976ef2a5eb243e297a2827
Tyler Long <tyler4long@gmail.com>
2018-06-06 21:37:23 +0800

    Link to home page

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const folders = [
   [...new Array(20).keys()].map(i => `C${i + 1}`)
 ]
 
-fs.writeFileSync('index.html', nunjucks.render('index.html', { folders }))
+fs.writeFileSync('index.html', nunjucks.render('index.html', { folders, root: '.' }))
 
 const letters = ['A', 'B', 'C']
 const length = { A: 10, B: 15, C: 20 }
@@ -17,6 +17,6 @@ const length = { A: 10, B: 15, C: 20 }
 for (const letter of letters) {
   for (let i = 0; i < length[letter]; i++) {
     const folder = `${letter}/${letter}${i + 1}`
-    fs.writeFileSync(`${folder}.html`, nunjucks.render('letter.html', { folder }))
+    fs.writeFileSync(`${folder}.html`, nunjucks.render('letter.html', { folder, root: '../..' }))
   }
 }
