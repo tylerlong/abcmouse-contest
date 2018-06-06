622435c29007f1da53a7a2f409583d4a024c28fd
Tyler Long <tyler4long@gmail.com>
2018-06-06 23:08:38 +0800

    Get words and sentence done

--- index.js
+++ index.js
@@ -5,17 +5,32 @@ import path from 'path'
 nunjucks.configure('views', { autoescape: true })
 
 const getDirs = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isDirectory())
+const getFiles = p => fs.readdirSync(p).filter(f => fs.statSync(path.join(p, f)).isFile())
 
-fs.writeFileSync('index.html', nunjucks.render('index.html', { root: '.' }))
+const letters = ['A', 'B', 'C']
+const dirss = letters.map(l => getDirs(`materials/${l}`))
+const filesss = dirss.map(dirs => {
+  return dirs.map(dir => {
+    return getFiles(`materials/${dir[0]}/${dir}`).filter(f => f.toLowerCase().endsWith('.jpg') || f.toLowerCase().endsWith('.bmp') || f.toLowerCase().endsWith('.png'))
+  })
+})
 
-for (const letter of ['A', 'B', 'C']) {
-  const dirs = getDirs(`materials/${letter}`)
-  for (const dir of dirs) {
+fs.writeFileSync('index.html', nunjucks.render('index.html', { root: '.', letters, dirss }))
+
+for (let i = 0; i < letters.length; i++) {
+  const letter = letters[i]
+  const dirs = dirss[i]
+  const filess = filesss[i]
+  for (let j = 0; j < dirs.length; j++) {
+    const dir = dirs[j]
+    const files = filess[j]
     fs.writeFileSync(`${letter}/${dir}.html`, nunjucks.render('letter.html', {
       letter,
+      dirs,
       dir,
-      root: '../..',
-      length: dirs.length
+      words: files.slice(0, files.length - 1),
+      sentence: files[files.length - 1],
+      root: '../..'
     }))
   }
 }
