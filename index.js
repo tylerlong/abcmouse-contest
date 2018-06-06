e53e22ca9942a3e0124e1f1320d631f98de22948
Tyler Long <tyler4long@gmail.com>
2018-06-06 21:34:18 +0800

    Generate all the pages

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ const folders = [
 ]
 
 fs.writeFileSync('index.html', nunjucks.render('index.html', { folders }))
+
+const letters = ['A', 'B', 'C']
+const length = { A: 10, B: 15, C: 20 }
+
+for (const letter of letters) {
+  for (let i = 0; i < length[letter]; i++) {
+    const folder = `${letter}/${letter}${i + 1}`
+    fs.writeFileSync(`${folder}.html`, nunjucks.render('letter.html', { folder }))
+  }
+}
